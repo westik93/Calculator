0108825374fdca33ac39230075d1f4fe9e9d2a9d
Khongor Khodjigorov <westik08@mail.ru>
2020-12-18 01:29:04 +0300

    third commit

--- calc.py
+++ calc.py
@@ -5,12 +5,18 @@ from colorama import Fore, Back, Style
 
 init()
 
+print(Fore.BLACK)
 print(Back.GREEN)
- 
+
 what = input('Какую математическую операцию хотите произвести? (+, -, *, /):')
 
+print(Back.CYAN)
+
 a = float(input('Введите первое число: '))
+print(Back.RED)
 b = float(input('Введите второе число: '))
+
+print(Back.YELLOW)
 
 if what == '+':
 	c = a + b
